81b046305d62067da6335f9906c122336fbe7670
Jose Barrios <jose@cognilab.com>
2017-12-11 22:40:50 -0800

    Basic EmailMessage Data Controller with very basic sender, recipient, about, text, description and dateSent properties (for now)

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const Message = require('@josebarrios/message')
+const MessageDataController = require('dc-message')
 
-class EmailMessage extends Message{
+class EmailMessageDataController extends MessageDataController {
 
-    constructor(model){
-      model = model || {};
-      super(model)
+	constructor(model){
+		model = model || {};
+		super(model)
 
-      //Non-standard
-      this.text = model.text;
-      this.HTML = model.HTML;
-    }
+		this.sender = model.sender;
+		this.recipient = model.recipient;
+		this.about = model.about;
+		this.text = model.text;
+		this.description = model.description;
+		this.dateSent = model.dateSent;
+	}
 
-  get text(){ return this.computed.text }
-  set text(value){ this.computed.text = value }
+	get sender(){ return this.model.sender }
+	set sender(value){ this.model.sender = value }
 
-  get html(){ return this.computed.HTML }
-  set html(value){ this.computed.HTML = value }
-  get HTML(){ return this.computed.HTML }
-  set HTML(value){ this.computed.HTML = value }
+	get recipient(){ return this.model.recipient }
+	set recipient(value){ this.model.recipient = value }
+
+	get about(){ return this.model.about }
+	set about(value){ this.model.about = value }
+
+	get text(){ return this.model.text }
+	set text(value){ this.model.text = value }
+
+	get description(){ return this.model.description }
+	set description(value){ this.model.description = value }
+
+	get dateSent(){ return this.model.dateSent }
+	set dateSent(value){ this.model.dateSent = value }
 }
 
-module.exports = EmailMessage;
+module.exports = EmailMessageDataController;
--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,68 @@
 'use strict'
 
 var assert = require('assert');
-var EmailMessage = require('../index.js');
+var EmailMessageDataController = require('../index.js');
 
 var model = {};
-let emailMessage = new EmailMessage(model);
+model.sender = {};
+model.givenName = 'Jose';
+model.familyName = 'Barrios';
+model.email = 'email@email.com';
 
-describe('EmailMessage\n', function() {
+model.recipient = {};
+model.givenName = 'Jane';
+model.familyName = 'Smith';
+model.email = 'jane@smith.com';
+
+
+model.about = 'Test Email';
+model.text = '<h1 style="color:red"> HTML Email </h1>';
+model.description = 'Text Email';
+model.dateSent = Date.now();
+
+let email = new EmailMessageDataController(model);
+
+describe('EmailMessageDataController\n', function() {
   describe('Static Methods:', function() {
-    describe('EmailMessage.isNumber()', function() {
+    describe('EmailMessageDataController.isNumber()', function() {
       it('should ...', function() {
-        assert.equal(EmailMessage.isNumber(1), true);
-        assert.equal(EmailMessage.isNumber(''), false);
+        assert.equal(EmailMessageDataController.isNumber(1), true);
+        assert.equal(EmailMessageDataController.isNumber(''), false);
       });
     });
+  });
+
+  describe('Properties:', function() {
+    describe('sender', function() {
+			it('should be person', function() {
+        assert.deepEqual(email.sender, model.sender);
+      });
+    });
+    describe('recipient', function() {
+			it('should be person', function() {
+        assert.deepEqual(email.recipient, model.recipient);
+      });
+    });
+    describe('about', function() {
+			it('should be text', function() {
+        assert.equal(email.about, model.about);
+      });
+    });
+    describe('text', function() {
+			it('should be HTML text', function() {
+        assert.equal(email.text, model.text);
+      });
+    });
+    describe('description', function() {
+			it('should be text', function() {
+        assert.equal(email.description, model.description);
+      });
+    });
+
+    //describe('dateSent', function() {
+			//it('should be date', function() {
+        //assert.equal(email.sender, false);
+      //});
+    //});
   });
 });
